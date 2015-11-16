3bbc5a4d4c36dd25eb55fd995498fe55529a9e6c
Jacky Fong <jacky.fong@edu.gobelins.fr>
2015-11-16 17:23:22 +0100

    Add Cursor event

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight,{backgroundColor : 0x000000, antialias: true});
+var renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight,{backgroundColor : 0x000000, antialias: true});
 renderer.view.style.position = 'absolute';
 renderer.view.style.display = 'block';
 document.body.appendChild(renderer.view);
@@ -13,7 +13,7 @@ function Node(radius, speed, color) {
 
     this.pixi.x = x;
     this.pixi.y = y;
-    this.pixi.alpha = 0;
+    //this.pixi.alpha = 1;
 
     this.pixi.beginFill(color);
     this.pixi.drawCircle(0, 0, radius);
@@ -29,23 +29,25 @@ function Node(radius, speed, color) {
 
             this.pixi.x = x;
             this.pixi.y = y;
-            this.pixi.alpha = 0;
+            //this.pixi.alpha = 0;
         }
-        else if(this.pixi.alpha > 1) {
-            this.pixi.x += Math.sin(this.direction * Math.PI / 180) * this.speed;
-            this.pixi.y += Math.cos(this.direction * Math.PI / 180) * this.speed;
-        }
+        //else if(this.pixi.alpha > 1) {
+        //    this.pixi.x += Math.sin(this.direction * Math.PI / 180) * this.speed;
+        //    this.pixi.y += Math.cos(this.direction * Math.PI / 180) * this.speed;
+        //}
         else {
             this.pixi.x += Math.sin(this.direction * Math.PI / 180) * this.speed;
             this.pixi.y += Math.cos(this.direction * Math.PI / 180) * this.speed;
-            this.pixi.alpha += 0.01;
+        //    this.pixi.alpha += 0.01;
         }
     };
 
 }
 
+var mouseData = {};
 var stage = new PIXI.Container();
-var nodesNumber = 500;
+stage.interactive = true;
+var nodesNumber = 400;
 var nodes = [];
 
 var lines = new PIXI.Graphics();
@@ -58,7 +60,7 @@ stage.addChild(lines);
 for(var i = 0; i < nodesNumber; i++) {
 
     var radius = 0.5;
-    var speed = 1;
+    var speed = 0.8;
     var color = 0xFFFFFF;
 
     var node = new Node(radius, speed, color);
@@ -79,10 +81,8 @@ function animate(){
         for (var j = 0; j <= nodes.length - 1; j++) {
 
             if(!(nodes[i] == nodes[j])) {
-                var dx = nodes[i].pixi.x - nodes[j].pixi.x;
-                var dy = nodes[i].pixi.y - nodes[j].pixi.y;
-                var distance = Math.sqrt(( dx * dx ) + ( dy * dy ));
 
+                var distance = getDistance(nodes[i].pixi.x, nodes[i].pixi.y, nodes[j].pixi.x, nodes[j].pixi.y);
 
                 if (distance < drawLineDistance) {
                     //nodes[i].pixi.speed = nodes[i].pixi.speed + distance;
@@ -93,6 +93,18 @@ function animate(){
             }
 
         }
+    }
+
+    for (var j = 0; j <= nodes.length - 1; j++) {
+
+        var distance = getDistance(mouseData.x, mouseData.y, nodes[j].pixi.x, nodes[j].pixi.y);
+
+        if (distance < 120) {
+        lines.lineStyle(1, 0xFFFFFF, ((120 - distance) / distance)); //0xB9B4B8
+        lines.moveTo(mouseData.x, mouseData.y);
+        lines.lineTo(nodes[j].pixi.x, nodes[j].pixi.y);
+        }
+
     }
 
     renderer.render(stage);
@@ -106,6 +118,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function getDistance(x1, y1, x2, y2){
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    var distance = Math.sqrt(( dx * dx ) + ( dy * dy ));
+
+    return distance;
+}
+
 window.addEventListener('resize', resize);
 
 function resize() {
@@ -114,26 +134,7 @@ function resize() {
 
 
 
-mySprite.mousemove = function(mouseData)
+stage.mousemove = function()
 {
-    // this line will get the mouse coords relative to the sprites..
-    var localCoordsPosition = mouseData.getLocalPosition(mySprite);
-
-    // this line will get the mouse coords relative to the sprites parent..
-    var parentCoordsPosition = mouseData.getLocalPosition(mySprite.parent);
-
-    this.position.x = parentCoordsPosition.x;
-    this.position.y = parentCoordsPosition.y;
-}
-
-mySprite.mousemove = function(mouseData)
-{
-    // this line will get the mouse coords relative to the sprites..
-    var localCoordsPosition = mouseData.getLocalPosition(mySprite);
-
-    // this line will get the mouse coords relative to the sprites parent..
-    var parentCoordsPosition = mouseData.getLocalPosition(mySprite.parent);
-
-    this.position.x = parentCoordsPosition.x;
-    this.position.y = parentCoordsPosition.y;
+    mouseData = renderer.plugins.interaction.mouse.global;
 }
